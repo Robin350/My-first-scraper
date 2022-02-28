22f731b0d61e142fc6d935f8b0e001a67edf4a40
Robin <robin.costas@wazuh.com>
2022-02-28 14:31:28 +0100

    Remove unused code

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
 #!/usr/bin/env python
 
-import os
-import requests
-
-from requests.auth import HTTPBasicAuth
-from googletrans import Translator, constants
-
-from bs4 import BeautifulSoup
-
 #################################
 # from routes.textbook_api import textbook_api
 # from flask import Flask
 
 # Flask
 # app = Flask(__name__)
-#
-# xray_recorder.configure(service="Textbook scraper API")
-# app.register_blueprint(environments_endpoint, url_prefix="/<version>")
 #################################
 
 
